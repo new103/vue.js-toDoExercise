55b422e29f60f4b96581d6e4e1bdbd30811699de
new103 <waligora.t@gmail.com>
2021-03-06 13:42:10 +0100

    v2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <h1>ToDo Vue</h1>
-    <input type="text" v-model="newToDo" />
+    <input type="text" v-model="newToDo" v-on:keyup.enter="add" />
     <button v-on:click="add()">Add</button>
     <ul>
       <li style="list-style-type: none" v-for="(todo, i) in existingToDo">
@@ -19,24 +19,23 @@ export default {
       newToDo: "",
       existingToDo: [
         { text: "Shopping", id: 1 },
-        { text: "Projects", id: 2 },
-      ],
+        { text: "Projects", id: 2 }
+      ]
     };
   },
   methods: {
     add() {
       this.existingToDo.push({
         text: this.newToDo,
-        id: new Date().valueOf(),
+        id: new Date().valueOf()
       }),
         (this.newToDo = "");
     },
     deleteToDo(i) {
       this.existingToDo.splice(i, 1);
-    },
-  },
+    }
+  }
 };
 </script>
 
-<style>
-</style>
+<style></style>
